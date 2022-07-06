504928f4b31b84337d34f928b507ab06edafd33e
marywind <shadowind1285@gmail.com>
2022-07-06 16:14:35 +0300

    Version 0.2

---
+++ src/main/java/com/company/test/entity/Deals.java
@@ -0,0 +1,184 @@
+package com.company.test.entity;
+
+import io.jmix.core.annotation.DeletedBy;
+import io.jmix.core.annotation.DeletedDate;
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "DEALS", indexes = {
+        @Index(name = "IDX_DEALS_CLIENT_ID", columnList = "CLIENT_ID"),
+        @Index(name = "IDX_DEALS_CAR_ID", columnList = "CAR_ID")
+})
+@Entity
+public class Deals {
+    @InstanceName
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @Column(name = "DEAL_DATE", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date deal_date;
+
+    @Column(name = "RETURN_DATE_EXPECTED", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date return_date_expected;
+
+    @Column(name = "RETURN_DATE_ACTUAL")
+    @Temporal(TemporalType.DATE)
+    private Date return_date_actual;
+
+    @JoinColumn(name = "CLIENT_ID", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Clients client;
+
+    @JoinColumn(name = "CAR_ID", nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private Cars car;
+
+    @Column(name = "VERSION", nullable = false)
+    @Version
+    private Integer version;
+
+    @CreatedBy
+    @Column(name = "CREATED_BY")
+    private String createdBy;
+
+    @CreatedDate
+    @Column(name = "CREATED_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @LastModifiedBy
+    @Column(name = "LAST_MODIFIED_BY")
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    @Column(name = "LAST_MODIFIED_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @DeletedBy
+    @Column(name = "DELETED_BY")
+    private String deletedBy;
+
+    @DeletedDate
+    @Column(name = "DELETED_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date deletedDate;
+
+    public Cars getCar() {
+        return car;
+    }
+
+    public void setCar(Cars car) {
+        this.car = car;
+    }
+
+    public Clients getClient() {
+        return client;
+    }
+
+    public void setClient(Clients client) {
+        this.client = client;
+    }
+
+    public Date getReturn_date_actual() {
+        return return_date_actual;
+    }
+
+    public void setReturn_date_actual(Date return_date_actual) {
+        this.return_date_actual = return_date_actual;
+    }
+
+    public Date getReturn_date_expected() {
+        return return_date_expected;
+    }
+
+    public void setReturn_date_expected(Date return_date_expected) {
+        this.return_date_expected = return_date_expected;
+    }
+
+    public Date getDeal_date() {
+        return deal_date;
+    }
+
+    public void setDeal_date(Date deal_date) {
+        this.deal_date = deal_date;
+    }
+
+    public Date getDeletedDate() {
+        return deletedDate;
+    }
+
+    public void setDeletedDate(Date deletedDate) {
+        this.deletedDate = deletedDate;
+    }
+
+    public String getDeletedBy() {
+        return deletedBy;
+    }
+
+    public void setDeletedBy(String deletedBy) {
+        this.deletedBy = deletedBy;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
