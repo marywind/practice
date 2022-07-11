a1ff23b3c7205212842f2fb5487a5bda75eca022
marywind <shadowind1285@gmail.com>
2022-07-11 23:50:45 +0300

    Version 0.7

--- src/main/java/com/company/test/screen/deals/DealsBrowseCars.java
+++ src/main/java/com/company/test/screen/deals/DealsBrowseCars.java
@@ -1,13 +1,21 @@
 package com.company.test.screen.deals;
 
 import com.company.test.entity.Deals;
-import io.jmix.ui.screen.LookupComponent;
-import io.jmix.ui.screen.StandardLookup;
-import io.jmix.ui.screen.UiController;
-import io.jmix.ui.screen.UiDescriptor;
+import io.jmix.core.DataManager;
+import io.jmix.ui.component.GroupTable;
+import io.jmix.ui.screen.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("Deals.browse.cars")
 @UiDescriptor("deals-browse-cars.xml")
 @LookupComponent("dealsesTable")
 public class DealsBrowseCars extends StandardLookup<Deals> {
+
+    @Install(to = "dealsesTable", subject = "styleProvider")
+    private String dealsesTableStyleProvider(Deals deals, String property) {
+        if (property == null) {
+            if (deals.getReturn_date_actual()==null) return "green-test";
+        }
+        return null;
+    }
 }
